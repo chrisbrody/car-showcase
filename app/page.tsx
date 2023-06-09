821b7d2745ee0a905612553499139ac3a9238b84
chrisbrody <chrisbrody@codifyacademy.com>
2023-06-09 13:55:58 -0400

    feat: initial project code created

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+// custom components
+import { Hero } from '@/components'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <Hero />
+    </main>
+  )
+}
