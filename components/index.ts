acf01966aeec6e4a2da52e564f85921546dbe5c6
chrisbrody <chrisbrody@codifyacademy.com>
2023-06-12 09:27:47 -0400

    feat: added customfilter and searchbar compontent shells

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,15 @@
+import NavBar from "./NavBar";
 import Hero from './Hero';
 import CustomButton from "./CustomButton";
-import NavBar from "./NavBar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
 import Footer from "./Footer";
 
 export {
+    NavBar,
     Hero, 
     CustomButton,
-    NavBar,
+    CustomFilter,
+    SearchBar,
     Footer,
 }
